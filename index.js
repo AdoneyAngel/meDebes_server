ef0c8d98240d21029bab83576a3e55ab9bd84884
AdoneyAngel <Adoneyangeltj@gmail.com>
2024-06-16 20:14:51 +0100

    arreglado getUserByMail

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/getUsers", (req, res) => {
     })
 })
 
-app.post("/getUserByMail", (req, res) => {
+app.get("/getUserByMail", (req, res) => {
     const mail = req.body.mail
 
     if (mail) {
