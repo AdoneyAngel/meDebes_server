be63a4e90c2ba6557704cff504c3987f323b23ab
AdoneyAngel <Adoneyangeltj@gmail.com>
2024-06-16 14:38:04 +0100

    Conexion a base de datos remota

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config()
+
+const mysql = require("mysql")
+const express = require("express")
+const cors = require("cors")
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}, (err) => {
+    if (err) {
+        console.log(err)
+
+    } else {
+        console.log("Database conected")
+    }
+})
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(3001, (err, res) => {
+    console.log("Running in: " + 3001)
+})
+
+//METHODS
+
+app.get("/getUsers", (req, res) => {
+    db.query("CALL sp_medebes_users_select()", (err, queryRes) => {
+        if (err) {
+            console.log(err)
+
+        } else {
+           res.send(queryRes[0]) 
+        }  
+    })
+})
