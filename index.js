89c305817c168da787c5b8fc1d50b62122543d8a
AdoneyAngel <Adoneyangeltj@gmail.com>
2024-06-17 17:30:27 +0100

    abrir y cerrar conexion por cada operacion

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ app.listen(3001, (err, res) => {
 //METHODS
 
 app.get("/getUsers", (req, res) => {
+    db.connect()
+
     db.query("CALL sp_medebes_users_select()", (err, queryRes) => {
         if (err) {
             console.log(err)
@@ -37,6 +39,8 @@ app.get("/getUsers", (req, res) => {
         } else {
            res.send(queryRes) 
         }  
+
+        db.end()
     })
 })
 
@@ -44,6 +48,8 @@ app.post("/getUserByName", (req, res) => {
     const name = req.body.name
 
     if (name) {
+        db.connect()
+        
         db.query("CALL sp_medebes_users_select_name(?)", [name], (err, queryRes) => {
             if (err) {
                 console.log(err)
@@ -51,11 +57,14 @@ app.post("/getUserByName", (req, res) => {
             } else {
                 res.send(queryRes)
             }
+
+            db.end()
         })
     }
 })
 
 app.post("/getUserByMail", (req, res) => {
+    db.connect()
     const mail = req.body.mail
 
     if (mail) {
@@ -66,12 +75,15 @@ app.post("/getUserByMail", (req, res) => {
         } else {
             res.send(queryRes)
         }
+
+        db.end()
       })  
     }
     
 })
 
 app.post("/createUser", async (req, res) => {
+    db.connect()
     const name = req.body.name
     const mail = req.body.mail
     const password = req.body.password
@@ -89,10 +101,14 @@ app.post("/createUser", async (req, res) => {
             } else {
                 res.send(true)
             }
+
+            db.end()
         })
 
     } else {
         res.send(false)
+
+        db.end()
     }
 })
 
@@ -101,6 +117,7 @@ app.post("/login", async (req, res) => {
     const password = req.body.password
 
     if (mail, password) {
+        db.connect()
         db.query("CALL sp_medebes_users_select_mail(?)", [mail], async (err, queryRes) => {
             if (err) {
                 console.log(err)
@@ -123,6 +140,8 @@ app.post("/login", async (req, res) => {
             } else {
                 res.send(false)
             }
+
+            db.end()
         })
 
     } else {
