a16477ad7d6788bba2866b399e6b7a063c75d279
AdoneyAngel <Adoneyangeltj@gmail.com>
2024-06-17 15:45:20 +0100

    prueba

--- index.js
+++ index.js
@@ -105,8 +105,9 @@ app.post("/login", (req, res) => {
 
             } else if (queryRes) {
                 console.log("respuesta de la query: ")
+
                 queryRes.forEach(resItem => {
-                    console.log(resItem.name)
+                    console.log(resItem)
                 })
 
                 if (queryRes[0].length > 0) {
